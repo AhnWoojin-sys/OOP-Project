b2b55be35d1c6d4295931133af287b870d95cf9c
Woojin Ahn <woojin.ahn@stndenergy.com>
2020-10-23 20:31:41 +0900

    Added Function

--- BankingSystem.cpp
+++ BankingSystem.cpp
@@ -3,3 +3,23 @@
 
 using namespace std;
 
+void ShowMenu(void){
+    cout<<"-------- Menu --------"<<endl;
+    cout<<"1. Make Account"<<endl;
+    cout<<"2. DepositMoney"<<endl;
+    cout<<"3. WithdrawMoney"<<endl;
+    cout<<"4. Print All Acount Information"<<endl;
+    cout<<"5. Exit"<<endl;
+}
+void MakeAccount(void){
+
+}
+void DepositMoney(void){
+
+}
+void WithdrawMoney(void){
+
+}  
+void ShowAllAccInfo(void){
+
+} //Show All Account info
--- main.cpp
+++ main.cpp
@@ -4,7 +4,27 @@
 using namespace std;
 
 int main(void){
-    
-
+    int choice;
+    ACCOUNT ac[100];
+    int accNum = 0;
+    while(true){
+        ShowMenu();
+        cout<<"Select : ";
+        cin>>choice;
+        switch(choice){
+            case MAKE:
+                break;
+            case DEPOSIT:
+                break;
+            case WITHDRAW:
+                break;
+            case INQUIRE:
+                break;
+            case EXIT:
+                cout<<"bye."<<endl;
+                return 0;
+                break;
+        }
+    }
     return 0;
 }
